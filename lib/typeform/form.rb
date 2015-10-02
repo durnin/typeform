c93db05f580ce366b55ae2279d5da558a3fa9d48
Rodrigo Martinez <durnin83@gmail.com>
2015-10-02 16:51:13 -0300

    Changed hashie version dependency and complete_entries method.

--- lib/typeform/form.rb
+++ lib/typeform/form.rb
@@ -16,8 +16,14 @@ module Typeform
     end
 
     def complete_entries(params = {})
+      #response = get(params.merge(completed: true))
+      #Hashie::Mash.new(response)
       response = get(params.merge(completed: true))
-      Hashie::Mash.new(response)
+      response_hashie = Hashie::Mash.new(response)
+      id_to_question_mapper = get_id_to_question_mapper(response_hashie.questions)
+      Struct.new("AllForms", :answers, :questions_options_size)
+      Struct::AllForms.new(get_forms_answers(response_hashie.responses, id_to_question_mapper),
+                           get_questions_options_size(response_hashie.questions))
     end
 
     def incomplete_entries(params = {})
@@ -34,6 +40,35 @@ module Typeform
 
       def uri
         "/form/#{form_id}"
+      end
+
+      def get_question_string(questions, question, group_separator = '|')
+        if question.key?('group')
+          recursive_question = questions.find {|q| q.id == question.group}
+          get_question_string(questions, recursive_question) + group_separator + question.question
+        else
+          question.question
+        end
+      end
+
+      def get_id_to_question_mapper(questions)
+        Hash[questions.collect { |q| [q.id, get_question_string(questions, q)] }]
+      end
+
+      def get_forms_answers(responses, id_to_question_mapper)
+        forms_answers = []
+        responses.each do |response|
+          form_answers = Hash.new { |hash, key| hash[key] = Array.new }
+          response.answers.each do |id, value|
+            form_answers[id_to_question_mapper[id]] << value unless value.nil? || value.empty?
+          end
+          forms_answers << form_answers
+        end
+        forms_answers
+      end
+
+      def get_questions_options_size(questions)
+        questions.each_with_object(Hash.new(0)) { |key, hash| hash[get_question_string(questions, key)] += 1 }
       end
   end
 
